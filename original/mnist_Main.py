3f3ebfad4c0d82adcea3531b296f1b4d21b9fb93
Belis0811 <anzheche@usc.edu>
2023-08-12 18:57:31 -0700

    update mnist_Main.py

--- original/mnist_Main.py
+++ original/mnist_Main.py
@@ -16,6 +16,10 @@ transform = transforms.Compose([
 batch_size = 64
 trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
+
+testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False)
+
 # init the model
 num_classes = 10
 resnet34 = ResNet.ResNet34(num_classes=num_classes)
@@ -28,6 +32,7 @@ optimizer = optim.SGD(resnet34.parameters(), lr=0.001, momentum=0.9)  # update f
 criterion = torch.nn.CrossEntropyLoss()
 
 train_losses = []
+test_losses = []
 # train
 num_epochs = 20
 for epoch in range(num_epochs):
@@ -49,12 +54,25 @@ for epoch in range(num_epochs):
 
     print(f"Epoch [{epoch + 1}/{num_epochs}], Loss: {running_loss / len(trainloader):.4f}")
 
+    # test in every epoch
+    resnet34.eval()
+    test_running_loss = 0.0
+    with torch.no_grad():
+        for test_inputs, test_labels in testloader:
+            test_inputs, test_labels = test_inputs.to(device), test_labels.to(device)
+
+            test_outputs = resnet34(test_inputs)
+            test_loss = criterion(test_outputs, test_labels)
+            test_running_loss += test_loss.item()
+
+    avg_test_loss = test_running_loss / len(testloader)
+    test_losses.append(avg_test_loss)
+    print(f"Epoch [{epoch + 1}/{num_epochs}], Test Loss: {avg_test_loss:.4f}")
+
 print("Training finished!")
 
 # test
 resnet34.eval()
-testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False)
 
 all_labels = []
 all_predictions = []
@@ -72,7 +90,15 @@ accuracy = accuracy_score(all_labels, all_predictions)
 print(f"Test Accuracy: {accuracy:.2f}")
 
 # plot the learning loss
+'''
 plt.plot(train_losses, label="Train Loss")
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.legend()
+plt.show()
+'''
+
+plt.plot(test_losses, label="Test Loss")
 plt.xlabel("Epoch")
 plt.ylabel("Loss")
 plt.legend()
