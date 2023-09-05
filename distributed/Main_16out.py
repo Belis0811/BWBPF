a16ff05982f1f33d8d723734fe5e0b30e759092b
Belis0811 <anzheche@usc.edu>
2023-09-05 15:06:40 -0700

    update tinyimagenet

---
+++ distributed/Main_16out.py
@@ -0,0 +1,307 @@
+'''Train CIFAR10 with PyTorch.'''
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.backends.cudnn as cudnn
+import torchvision
+import torchvision.transforms as transforms
+import ResNet_16out
+import tiny_imagenet_loader
+
+device_id = 0
+device = torch.device('cuda:{}'.format(device_id)) if torch.cuda.is_available() else 'cpu'
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+
+# get directory
+train_directory = '../tiny-imagenet-200/train'
+test_directory = '../tiny-imagenet-200/'
+# Data
+transform_train = transforms.Compose([
+    transforms.RandomCrop(64, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+trainset = torchvision.datasets.ImageFolder(
+    root=train_directory, transform=transform_train)
+trainloader = torch.utils.data.DataLoader(
+    trainset, batch_size=32, shuffle=True, num_workers=0)
+
+testset = tiny_imagenet_loader.TinyImageNet_load(test_directory, train=False, transform=transform_test)
+testloader = torch.utils.data.DataLoader(
+    testset, batch_size=32, shuffle=False, num_workers=0)
+
+# load resnet101
+net = ResNet_16out.ResNet101(num_classes=200)
+net = net.to(device)
+if device == 'cuda':
+    net = torch.nn.DataParallel(net)
+    cudnn.benchmark = True
+
+criterion = nn.CrossEntropyLoss()
+# define optimizer and loss function
+optimizer_1 = optim.SGD([
+    {'params': net.conv1.parameters()},
+    {'params': net.bn1.parameters()},
+    {'params': net.layer1.parameters()},
+    {'params': net.fc1.parameters()}
+], lr=0.1, momentum=0.9, weight_decay=5e-4)  # update first two layer
+
+optimizer_2 = optim.SGD([
+    {'params': net.layer2.parameters()},
+    {'params': net.fc2.parameters()}
+], lr=0.1, momentum=0.9, weight_decay=5e-4)  # update layer3 and 4
+
+optimizer_3 = optim.SGD([
+    {'params': net.layer3.parameters()},
+    {'params': net.fc3.parameters()}
+], lr=0.1, momentum=0.9, weight_decay=5e-4)  # update layer3 and 4
+
+optimizer_4 = optim.SGD([
+    {'params': net.layer4.parameters()},
+    {'params': net.fc4.parameters()}
+], lr=0.1, momentum=0.9, weight_decay=5e-4)  # update layer3 and 4
+
+optimizer_5 = optim.SGD([
+    {'params': net.layer5.parameters()},
+    {'params': net.fc5.parameters()}
+], lr=0.1, momentum=0.9, weight_decay=5e-4)  # update first two layer
+
+optimizer_6 = optim.SGD([
+    {'params': net.layer6.parameters()},
+    {'params': net.fc6.parameters()}
+], lr=0.1, momentum=0.9, weight_decay=5e-4)  # update layer3 and 4
+
+optimizer_7 = optim.SGD([
+    {'params': net.layer7.parameters()},
+    {'params': net.fc7.parameters()}
+], lr=0.1, momentum=0.9, weight_decay=5e-4)  # update layer3 and 4
+
+optimizer_8 = optim.SGD([
+    {'params': net.layer8.parameters()},
+    {'params': net.fc8.parameters()}
+], lr=0.1, momentum=0.9, weight_decay=5e-4)  # update layer3 and 4
+
+optimizer_9 = optim.SGD([
+    {'params': net.layer9.parameters()},
+    {'params': net.fc9.parameters()}
+], lr=0.1, momentum=0.9, weight_decay=5e-4)  # update first two layer
+
+optimizer_10 = optim.SGD([
+    {'params': net.layer10.parameters()},
+    {'params': net.fc10.parameters()}
+], lr=0.1, momentum=0.9, weight_decay=5e-4)  # update layer3 and 4
+
+optimizer_11 = optim.SGD([
+    {'params': net.layer11.parameters()},
+    {'params': net.fc11.parameters()}
+], lr=0.1, momentum=0.9, weight_decay=5e-4)  # update layer3 and 4
+
+optimizer_12 = optim.SGD([
+    {'params': net.layer12.parameters()},
+    {'params': net.fc12.parameters()}
+], lr=0.1, momentum=0.9, weight_decay=5e-4)  # update layer3 and 4
+
+optimizer_13 = optim.SGD([
+    {'params': net.layer13.parameters()},
+    {'params': net.fc13.parameters()}
+], lr=0.1, momentum=0.9, weight_decay=5e-4)  # update layer3 and 4
+
+optimizer_14 = optim.SGD([
+    {'params': net.layer14.parameters()},
+    {'params': net.fc14.parameters()}
+], lr=0.1, momentum=0.9, weight_decay=5e-4)  # update layer3 and 4
+
+optimizer_15 = optim.SGD([
+    {'params': net.layer15.parameters()},
+    {'params': net.fc15.parameters()}
+], lr=0.1, momentum=0.9, weight_decay=5e-4)  # update layer3 and 4
+
+optimizer_16 = optim.SGD([
+    {'params': net.layer16.parameters()},
+    {'params': net.fc16.parameters()}
+], lr=0.1, momentum=0.9, weight_decay=5e-4)  # update layer3 and 4
+
+scheduler_1 = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_1, T_max=200)
+scheduler_2 = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_2, T_max=200)
+scheduler_3 = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_3, T_max=200)
+scheduler_4 = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_4, T_max=200)
+scheduler_5 = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_5, T_max=200)
+scheduler_6 = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_6, T_max=200)
+scheduler_7 = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_7, T_max=200)
+scheduler_8 = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_8, T_max=200)
+scheduler_9 = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_9, T_max=200)
+scheduler_10 = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_10, T_max=200)
+scheduler_11 = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_11, T_max=200)
+scheduler_12 = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_12, T_max=200)
+scheduler_13 = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_13, T_max=200)
+scheduler_14 = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_14, T_max=200)
+scheduler_15 = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_15, T_max=200)
+scheduler_16 = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer_16, T_max=200)
+
+train_losses = []
+test_losses = []
+train_acc = []
+test_acc = []
+test_acc_best = 0
+epoch_acc_best = 0
+model_best = None
+epoch_num = 200
+
+
+# Training
+def train(epoch):
+    print('\nEpoch: %d' % epoch)
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    for inputs, targets in trainloader:
+        inputs, targets = inputs.to(device), targets.to(device)
+
+        optimizer_1.zero_grad()
+        optimizer_2.zero_grad()
+        optimizer_3.zero_grad()
+        optimizer_4.zero_grad()
+        optimizer_5.zero_grad()
+        optimizer_6.zero_grad()
+        optimizer_7.zero_grad()
+        optimizer_8.zero_grad()
+        optimizer_9.zero_grad()
+        optimizer_10.zero_grad()
+        optimizer_11.zero_grad()
+        optimizer_12.zero_grad()
+
+        outputs, extra_1, extra_2, extra_3, extra_4, extra_5, extra_6, extra_7, extra_8, extra_9, extra_10, extra_11, extra_12, extra_13, extra_14, extra_15 = net(
+            inputs)
+        loss_1 = criterion(extra_1, targets)
+        loss_1.backward(retain_graph=True)
+
+        loss_2 = criterion(extra_2, targets)
+        loss_2.backward(retain_graph=True)
+
+        loss_3 = criterion(extra_3, targets)
+        loss_3.backward(retain_graph=True)
+
+        loss_4 = criterion(extra_4, targets)
+        loss_4.backward(retain_graph=True)
+
+        loss_5 = criterion(extra_5, targets)
+        loss_5.backward(retain_graph=True)
+
+        loss_6 = criterion(extra_6, targets)
+        loss_6.backward(retain_graph=True)
+
+        loss_7 = criterion(extra_7, targets)
+        loss_7.backward(retain_graph=True)
+
+        loss_8 = criterion(extra_8, targets)
+        loss_8.backward(retain_graph=True)
+
+        loss_9 = criterion(extra_9, targets)
+        loss_9.backward(retain_graph=True)
+
+        loss_10 = criterion(extra_10, targets)
+        loss_10.backward(retain_graph=True)
+
+        loss_11 = criterion(extra_11, targets)
+        loss_11.backward(retain_graph=True)
+
+        loss_12 = criterion(extra_12, targets)
+        loss_12.backward(retain_graph=True)
+
+        loss_13 = criterion(extra_13, targets)
+        loss_13.backward(retain_graph=True)
+
+        loss_14 = criterion(extra_14, targets)
+        loss_14.backward(retain_graph=True)
+
+        loss_15 = criterion(extra_15, targets)
+        loss_15.backward(retain_graph=True)
+
+        loss_16 = criterion(outputs, targets)
+        loss_16.backward()
+
+        optimizer_1.step()
+        optimizer_2.step()
+        optimizer_3.step()
+        optimizer_4.step()
+        optimizer_5.step()
+        optimizer_6.step()
+        optimizer_7.step()
+        optimizer_8.step()
+        optimizer_9.step()
+        optimizer_10.step()
+        optimizer_11.step()
+        optimizer_12.step()
+        optimizer_13.step()
+        optimizer_14.step()
+        optimizer_15.step()
+        optimizer_16.step()
+
+        train_loss += loss_16.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+    print('Train Loss: %.3f | Acc: %.3f%% (%d/%d)'
+          % (train_loss / len(trainloader), 100. * correct / total, correct, total))
+    acc = 100. * correct / total
+    train_acc.append(acc)
+    train_losses.append(train_loss / len(trainloader))
+
+
+def test(epoch):
+    global test_acc_best
+    global epoch_acc_best
+    global model_best
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for inputs, targets in testloader:
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _ = net(inputs)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+    print('Test Loss: %.3f | Acc: %.3f%% (%d/%d)'
+          % (test_loss / len(testloader), 100. * correct / total, correct, total))
+    acc = 100. * correct / total
+    test_acc.append(acc)
+
+    if acc > test_acc_best:
+        test_acc_best = acc
+        epoch_acc_best = epoch
+        model_best = net.state_dict()
+
+    test_losses.append(test_loss / len(testloader))
+
+
+for epoch in range(start_epoch, start_epoch + epoch_num):
+    train(epoch)
+    test(epoch)
+    scheduler_1.step()
+    scheduler_2.step()
+    scheduler_3.step()
+    scheduler_4.step()
+    scheduler_5.step()
+    scheduler_6.step()
+    scheduler_7.step()
+    scheduler_8.step()
+
+# Save the trained weights
+save_path = 'resnet101_dis16_tinyImagenet.pth'
+torch.save(net.state_dict(), save_path)
+print("Trained weights saved to:", save_path)
---
+++ distributed/ResNet_16out.py
@@ -0,0 +1,247 @@
+'''ResNet in PyTorch.
+
+For Pre-activation ResNet, see 'preact_resnet.py'.
+
+Reference:
+[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
+    Deep Residual Learning for Image Recognition. arXiv:1512.03385
+'''
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.downsample = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.downsample = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion * planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.downsample(x)
+        out = F.relu(out)
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion *
+                               planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion * planes)
+
+        self.downsample = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.downsample = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion * planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.downsample(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=1000):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7,
+                               stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 256, num_blocks[3], stride=1)
+        self.layer5 = self._make_layer(block, 256, num_blocks[4], stride=1)
+        self.layer6 = self._make_layer(block, 256, num_blocks[5], stride=1)
+        self.layer7 = self._make_layer(block, 256, num_blocks[6], stride=1)
+        self.layer8 = self._make_layer(block, 256, num_blocks[7], stride=1)
+        self.layer9 = self._make_layer(block, 256, num_blocks[8], stride=1)
+        self.layer10 = self._make_layer(block, 256, num_blocks[9], stride=1)
+        self.layer11 = self._make_layer(block, 256, num_blocks[10], stride=1)
+        self.layer12 = self._make_layer(block, 256, num_blocks[11], stride=1)
+        self.layer13 = self._make_layer(block, 256, num_blocks[12], stride=1)
+        self.layer14 = self._make_layer(block, 256, num_blocks[13], stride=1)
+        self.layer15 = self._make_layer(block, 256, num_blocks[14], stride=1)
+        self.layer16 = self._make_layer(block, 512, num_blocks[15], stride=2)
+        self.fc1 = nn.Linear(64 * block.expansion, num_classes)
+        self.fc2 = nn.Linear(128 * block.expansion, num_classes)
+        self.fc3 = nn.Linear(256 * block.expansion, num_classes)
+        self.fc4 = nn.Linear(256 * block.expansion, num_classes)
+        self.fc5 = nn.Linear(256 * block.expansion, num_classes)
+        self.fc6 = nn.Linear(256 * block.expansion, num_classes)
+        self.fc7 = nn.Linear(256 * block.expansion, num_classes)
+        self.fc8 = nn.Linear(256 * block.expansion, num_classes)
+        self.fc9 = nn.Linear(256 * block.expansion, num_classes)
+        self.fc10 = nn.Linear(256 * block.expansion, num_classes)
+        self.fc11 = nn.Linear(256 * block.expansion, num_classes)
+        self.fc12 = nn.Linear(256 * block.expansion, num_classes)
+        self.fc13 = nn.Linear(256 * block.expansion, num_classes)
+        self.fc14 = nn.Linear(256 * block.expansion, num_classes)
+        self.fc15 = nn.Linear(256 * block.expansion, num_classes)
+        self.fc16 = nn.Linear(512 * block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+
+        out = self.layer1(out)
+        ex1 = out
+        ex1 = F.avg_pool2d(ex1, 20)
+        ex1 = ex1.view(ex1.size(0), -1)
+        ex1 = self.fc1(ex1)
+
+        out = self.layer2(out)
+        ex2 = out
+        ex2 = F.avg_pool2d(ex2, 10)
+        ex2 = ex2.view(ex2.size(0), -1)
+        ex2 = self.fc2(ex2)
+
+        out = self.layer3(out)
+        ex3 = out
+        ex3 = F.avg_pool2d(ex3, 8)
+        ex3 = ex3.view(ex3.size(0), -1)
+        ex3 = self.fc3(ex3)
+
+        out = self.layer4(out)
+        ex4 = out
+        ex4 = F.avg_pool2d(ex4, 8)
+        ex4 = ex4.view(ex4.size(0), -1)
+        ex4 = self.fc4(ex4)
+
+        out = self.layer5(out)
+        ex5 = out
+        ex5 = F.avg_pool2d(ex5, 8)
+        ex5 = ex5.view(ex5.size(0), -1)
+        ex5 = self.fc5(ex5)
+
+        out = self.layer6(out)
+        ex6 = out
+        ex6 = F.avg_pool2d(ex6, 8)
+        ex6 = ex6.view(ex6.size(0), -1)
+        ex6 = self.fc6(ex6)
+
+        out = self.layer7(out)
+        ex7 = out
+        ex7 = F.avg_pool2d(ex7, 8)
+        ex7 = ex7.view(ex7.size(0), -1)
+        ex7 = self.fc7(ex7)
+
+        out = self.layer8(out)
+        ex8 = out
+        ex8 = F.avg_pool2d(ex8, 8)
+        ex8 = ex8.view(ex8.size(0), -1)
+        ex8 = self.fc8(ex8)
+
+        out = self.layer9(out)
+        ex9 = out
+        ex9 = F.avg_pool2d(ex9, 8)
+        ex9 = ex9.view(ex9.size(0), -1)
+        ex9 = self.fc9(ex9)
+
+        out = self.layer10(out)
+        ex10 = out
+        ex10 = F.avg_pool2d(ex10, 8)
+        ex10 = ex10.view(ex10.size(0), -1)
+        ex10 = self.fc10(ex10)
+
+        out = self.layer11(out)
+        ex11 = out
+        ex11 = F.avg_pool2d(ex11, 8)
+        ex11 = ex11.view(ex11.size(0), -1)
+        ex11 = self.fc11(ex11)
+
+        out = self.layer12(out)
+        ex12 = out
+        ex12 = F.avg_pool2d(ex12, 8)
+        ex12 = ex12.view(ex12.size(0), -1)
+        ex12 = self.fc12(ex12)
+
+        out = self.layer13(out)
+        ex13 = out
+        ex13 = F.avg_pool2d(ex13, 8)
+        ex13 = ex13.view(ex13.size(0), -1)
+        ex13 = self.fc13(ex13)
+
+        out = self.layer14(out)
+        ex14 = out
+        ex14 = F.avg_pool2d(ex14, 8)
+        ex14 = ex14.view(ex14.size(0), -1)
+        ex14 = self.fc14(ex14)
+
+        out = self.layer15(out)
+        ex15 = out
+        ex15 = F.avg_pool2d(ex15, 8)
+        ex15 = ex15.view(ex15.size(0), -1)
+        ex15 = self.fc15(ex15)
+
+        out = self.layer16(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.fc16(out)
+
+        return out, ex1, ex2, ex3, ex4, ex5, ex6, ex7, ex8, ex9, ex10, ex11, ex12, ex13, ex14, ex15
+
+
+# def ResNet18(num_classes=1000):
+#     return ResNet(BasicBlock, [2, 2, 2, 2], num_classes)
+#
+#
+# def ResNet34(num_classes=1000):
+#     return ResNet(BasicBlock, [3, 4, 6, 3], num_classes)
+#
+#
+# def ResNet50(num_classes=1000):
+#     return ResNet(Bottleneck, [3, 4, 6, 3], num_classes)
+
+
+def ResNet101(num_classes=1000):
+    return ResNet(Bottleneck, [3, 4, 3, 2, 2, 2, 2, 2, 2, 2, 2, 3], num_classes)
+
+
+def ResNet152(num_classes=1000):
+    return ResNet(Bottleneck, [3, 8, 5, 4, 3, 3, 3, 3, 3, 3, 3, 3], num_classes)
+
+
+def test():
+    net = ResNet101()
+    y = net(torch.randn(1, 3, 32, 32))
+    print(y.size())
+
+# test()
