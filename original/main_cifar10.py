4b6ad9ef3460206669972c51c3acf8f1caad33e4
Anzhe Cheng <anzheche@usc.edu>
2023-09-14 13:45:16 -0700

    Update main_cifar10.py

--- original/main_cifar10.py
+++ original/main_cifar10.py
@@ -71,7 +71,7 @@ def train(epoch):
 
         optimizer.step()
 
-        train_loss += loss_back.item()
+        train_loss += loss.item()
         _, predicted = outputs.max(1)
         total += targets.size(0)
         correct += predicted.eq(targets).sum().item()
